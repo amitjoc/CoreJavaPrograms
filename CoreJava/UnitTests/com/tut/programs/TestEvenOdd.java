941ef6baf3bf18114a267755b3176f87d7761c61
Amit Joshi <amitjoc>
2017-12-15 01:44:11 +0530

    Two Test Case is written
    Case One: testForEven() which check for even number and true condition
    
    Case Two: testForOdd() which check for odd number and false condition

--- CoreJava/UnitTests/com/tut/programs/TestEvenOdd.java
+++ CoreJava/UnitTests/com/tut/programs/TestEvenOdd.java
@@ -6,13 +6,21 @@ import org.junit.jupiter.api.Test;
 
 class TestEvenOdd {
 
-	@Test
-	void testCheckEvenOdd() {
+		//Test for the Even Number
+		//return the true for even number 
+		@Test
+		public void testForEven() {
+			boolean TrueForEven = evenOdd.checkEvenOdd(4);
+			assertEquals(true, TrueForEven);
+		}
 		
-		//for the even test
-		 
-		boolean actual = evenOdd.checkEvenOdd(4);
-		assertEquals(true, actual);
-	}
-
-}
+		
+		//Test for the Odd number 
+		//return for the false for odd number
+		@Test
+		public void testForOdd() {
+			boolean falseForOdd = evenOdd.checkEvenOdd(5);
+			assertEquals(false, falseForOdd);
+		}
+		
+ }
