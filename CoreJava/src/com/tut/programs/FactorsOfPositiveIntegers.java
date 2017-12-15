af88c12b0eed3008c9c180618b41625b356e737a
Amit Joshi <amitjoc>
2017-12-15 16:54:58 +0530

    Written the method factorGenerator() takes number as argument for which we want to find the factors and returns the factorsArray back.
    Important Point:
    
    method initializes the internal array size with one extra size

---
+++ CoreJava/src/com/tut/programs/FactorsOfPositiveIntegers.java
@@ -0,0 +1,67 @@
+package com.tut.programs;
+
+import java.util.Scanner;
+
+/**
+ * <p>
+ * 		Question : Find all the factors of Given Positive Integer.<br>
+ *	 	Hint : if any number a divides any number b completely with 0 reminder {a<=b}
+ * </p> 
+ * @author Amit Joshi
+ * 
+  
+ */
+
+public class FactorsOfPositiveIntegers {
+	
+	/**
+	 * @author Amit Joshi
+	 *  
+	 * @param num	for which we want to genrate factors 
+	 */
+	
+	public static int[] factorGenrator(int num) {
+		
+		int[] factorsArray = new int[num+1]; // to hold the factors needed one extra memmory size
+		 
+		for(int loopCounter=1;loopCounter<=num;loopCounter++) {
+			if(num%loopCounter == 0) {
+				factorsArray[loopCounter]=loopCounter;
+			}else {
+				//do nothing or
+				factorsArray[loopCounter]=0; // no meaning because default values are zero
+			}
+		}
+	
+		
+		return factorsArray;
+	}
+		
+	
+	public static void main(String[] args) {
+		
+		int number, desiredFactorsArray [] ; // array is declared but initialized so size depands of return array
+		
+		System.out.println("Type the Number of Factors");
+		Scanner sc = new Scanner(System.in);
+		number = sc.nextInt();
+		
+		System.out.println("==================================");
+		desiredFactorsArray = factorGenrator(number);
+		
+		System.out.print("Desired Factors: [" );
+		
+		for(int lc=1;lc<=number;lc++) {
+			if(desiredFactorsArray[lc] ==0) continue;
+			System.out.print(desiredFactorsArray[lc] +" " );
+		}
+		
+		System.out.println("]");
+		
+		sc.close();
+		 
+	}
+}
+
+
+
