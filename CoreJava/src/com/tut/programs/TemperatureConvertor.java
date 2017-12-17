276941f3bf6e7964ba3c72f692c68ddc61c4499b
Admin <Admin@192.168.1.4>
2017-12-18 00:30:58 +0530

    Temperature Conversion

---
+++ CoreJava/src/com/tut/programs/TemperatureConvertor.java
@@ -0,0 +1,15 @@
+package com.tut.programs;
+
+public class TemperatureConvertor {
+
+	public static void main(String[] args) {
+		// TODO  Convert Given Temperature From Fahrenheit to Celsius
+		//  (°C  x  9/5) + 32 = °F
+		
+		
+		// TODO  Convert Given Temperature From Celsius to Fahrenheit 
+		//(°F  -  32)  x  5/9 = °C
+		
+	}
+
+}
