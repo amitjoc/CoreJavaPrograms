a1eb45afa65b0c2f2b17718cea6106850bf2fa86
Amit Joshi <amitjoc>
2017-12-17 23:04:32 +0530

    simple we have to just find the largest amoung the three

--- CoreJava/src/com/tut/programs/FindLargest.java
+++ CoreJava/src/com/tut/programs/FindLargest.java
@@ -1,5 +1,7 @@
 package com.tut.programs;
 
+import java.util.Scanner;
+
 public class FindLargest {
 	
 	
@@ -7,6 +9,22 @@ public class FindLargest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int num1,num2,num3;
+		int largest;
+		Scanner sc = new Scanner(System.in);
+		//read the three number
+		System.out.println("Enter three numbers:");
+		
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		num3 = sc.nextInt();
+		
+		largest   = num3 > (num1 > num2 ? num1 : num2) ? num3 : ((num1 > num2) ? num1 : num2);
+		
+		System.out.println("Largest number is :" + largest);
+		//close the scanner object
+		sc.close();
+		
 	}
 
 }
