7e2c359835908011bfe07ac49313cb697d8f8ab7
Amit Joshi <amitjoc@gmail.com>
2018-06-12 14:15:44 +0530

    Program to access the system properties.

---
+++ CoreJava/src/com/tut/sys/properties/AccessSystemProperties.java
@@ -0,0 +1,25 @@
+package com.tut.sys.properties;
+
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class AccessSystemProperties {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Getting the System Prperties");
+		System.out.println("-------------------------------");
+		
+		
+		Properties sysProperties = new Properties( System.getProperties());
+		Enumeration keys = sysProperties.propertyNames();
+		 
+		 while(keys.hasMoreElements()) {
+			 String key 	= 	(String)	keys.nextElement();
+			 String value 	= 	(String)	sysProperties.getProperty(key);
+			 System.out.println(key  + "------------>" +  value.trim());
+		 }
+	}
+
+}
