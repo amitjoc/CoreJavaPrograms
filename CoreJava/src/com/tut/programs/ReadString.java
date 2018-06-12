6656f605b97fd91d47ca0b5c1dd2a1e61dd3006b
Amit Joshi <amitjoc@gmail.com>
2018-06-12 12:39:52 +0530

    added the input message or alert "Please Enter the Number" so that user knows that

--- CoreJava/src/com/tut/programs/ReadString.java
+++ CoreJava/src/com/tut/programs/ReadString.java
@@ -9,21 +9,27 @@ public class ReadString {
 		String  bread;
 		  
 		 try {
-		 Scanner reader = new Scanner(System.in);
-		 
-		 if(reader.hasNextInt()) {
-
-			 System.out.println("Not Allowed");
-		 }else if(reader.hasNextLine()) {
-			 bread = reader.nextLine(); 
-			 System.out.println("Input is string" + bread );
-		 }else {
-			 System.out.println("input is not String");
-		 }
-			
+			 
+			 System.out.println("Please Enter the Number :");
+			 Scanner reader = new Scanner(System.in);
+			 
+			 if(reader.hasNextInt()) {
+	
+				 System.out.println("Not Allowed");
+			 }else if(reader.hasNextLine()) {
+				 bread = reader.nextLine(); 
+				 System.out.println("Input is string" + bread );
+			 }else {
+				 System.out.println("input is not String");
+			 }
+			 
+			 reader.close();
+				
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
+		 
+		 
 		 
 		 
 		
