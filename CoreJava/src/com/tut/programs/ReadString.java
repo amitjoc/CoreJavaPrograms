8c8fe2cab0779341bbdefb07523aae271f2cf1a9
Amit Joshi <amitjoc@gmail.com>
2018-06-13 13:35:14 +0530

    Alert message updated

--- CoreJava/src/com/tut/programs/ReadString.java
+++ CoreJava/src/com/tut/programs/ReadString.java
@@ -5,20 +5,19 @@ import java.util.Scanner;
 public class ReadString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Read the String from the console and read as integer or string as per method
 		String  bread;
 		  
 		 try {
 			 
-			 System.out.println("Please Enter the Number :");
+			 System.out.print("Please Enter the String or Number :");
 			 Scanner reader = new Scanner(System.in);
 			 
 			 if(reader.hasNextInt()) {
-	
-				 System.out.println("Not Allowed");
+				 System.out.println("Input is the Integer : " + reader.nextInt());
 			 }else if(reader.hasNextLine()) {
 				 bread = reader.nextLine(); 
-				 System.out.println("Input is string" + bread );
+				 System.out.println("Input is string : " + bread );
 			 }else {
 				 System.out.println("input is not String");
 			 }
