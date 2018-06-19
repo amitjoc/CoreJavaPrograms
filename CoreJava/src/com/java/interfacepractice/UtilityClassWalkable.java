7e7521043c08d40ad1a315f849a5504c971c255f
Amit Joshi <amitjoc@gmail.com>
2018-06-19 16:15:35 +0530

    Adding of method LetDuckWalk for duck walking

--- CoreJava/src/com/java/interfacepractice/UtilityClassWalkable.java
+++ CoreJava/src/com/java/interfacepractice/UtilityClassWalkable.java
@@ -14,9 +14,19 @@ public class UtilityClassWalkable {
 	 */
 	public static void LetThemWalk(Person[] personList) {
 		
+		//person clas solution
 		for(Person person: personList) {
 			person.walk();
 		}
+		
 	}
+	
+	public static void LetDuckWalk(Duck[] duckList) {
+		for(Duck duck: duckList) {
+			duck.walk();
+		}
+	}
+	
+	
 
 }
